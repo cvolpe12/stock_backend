6b908469afbdb23ee6133384d0b95c797426c07f
cvolpe12 <christopher.volpe12@gmail.com>
2019-10-02 11:34:09 -0400

    users can login or create new account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,45 @@
 class ApplicationController < ActionController::API
+  def encode_token(payload)
+   # byebug
+   JWT.encode(payload, "secret")
+   # ENV['KEY']
+   # payload {user_id: 1}
+   # => enaj.fewfwaf.dfadf
+ end
+
+ def decode_token
+   # if headers -> let's decode it else return nil
+   # JWT.decode()
+   begin
+     JWT.decode(auth_headers, "secret")
+   rescue
+     nil
+   end
+ end
+
+ def auth_headers
+   # check for headers
+   request.headers["Authorization"]
+   # returns the JWT
+ end
+
+ def curr_user
+   # byebug
+   user_id = decode_token[0]["user_id"]
+   User.find(user_id)
+   # who is the current user maybe we don't need to authorize the route but it might be nice to know
+   # who the user is to see whether or not they can validly access something
+ end
+
+ def logged_in
+   # is there a curr_user
+   !!curr_user
+ end
+
+ def authorized
+   # if there is a curr_user meaning they've been correctly validated
+   # allow them in our application
+   # otherwise send an error
+   render json: {errors: 'Please log in'} unless logged_in
+ end
 end
